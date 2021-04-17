2096418d37ddd3cdbeb386833cf8ee8e613e3edf
gogoend <gogoend@qq.com>
2021-04-17 17:05:55 +0800

    更新: 删除初始文件

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <app-main />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import AppMain from './views/AppMain/index.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    AppMain
   }
 });
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
