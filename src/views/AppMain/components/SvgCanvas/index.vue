6bfdf615d120451279af88caa6dbb31d744d3335
gogoend <gogoend@qq.com>
2021-04-24 11:11:49 +0800

    更新: 完善ghost连线逻辑，使连线起始点为circle元素右边缘，连线终止点为circle元素左边缘

--- src/views/AppMain/components/SvgCanvas/index.vue
+++ src/views/AppMain/components/SvgCanvas/index.vue
@@ -39,13 +39,6 @@ import IoNode from '@/components/IoNode/index.vue'
 const POINT_R = 10
 // 曲线手柄长度
 const HANDLE_LENGTH = 150
-// function mcPaser(path: string) {
-//   path = path + ''
-//   path = path.replace(/\s/g, '')
-//   path = path.replace(/[a-z]/i, '')
-//   path = path.replace(/[a-z]/ig, ',')
-//   return path
-// }
 
 export default defineComponent({
   name: 'SvgCanvas',
@@ -55,7 +48,6 @@ export default defineComponent({
   setup() {
     const linkedPath = ref([])
 
-    // const ghostPathDArguments = ref([850, 300, 1000, 300, 800, 400, 950, 400])
     const ghostPathDArguments = ref([0, 0, 0, 0, 0, 0, 0, 0])
     const ghostPathD = computed(() => {
       const dArgs = ghostPathDArguments.value
@@ -64,32 +56,82 @@ M ${dArgs[0]}, ${dArgs[1]}
 C ${dArgs[2]}, ${dArgs[3]}, ${dArgs[4]}, ${dArgs[5]}, ${dArgs[6]}, ${dArgs[7]}`
     })
 
-    const handlePortStart = ({ ev, originEl, vm }: {ev:MouseEvent, originEl: HTMLElement, vm: any}) => {
-      console.log(ev.target, originEl, vm)
-
-      ghostPathDArguments.value = [
-        ev.pageX + POINT_R, ev.pageY, ev.pageX + HANDLE_LENGTH, ev.pageY,
-        ev.pageX - HANDLE_LENGTH, ev.pageY, ev.pageX - POINT_R, ev.pageY
+    const handlePortStart = ({ ev, originEl, vm }: {ev:MouseEvent, originEl: SVGCircleElement, vm: any}) => {
+      const el = originEl
+      const coord = [
+        el.getBoundingClientRect().x,
+        el.getBoundingClientRect().y
       ]
+
+      if (el.classList.contains('in')) {
+        ghostPathDArguments.value = [
+          coord[0],
+          coord[1],
+          coord[0] + HANDLE_LENGTH,
+          coord[1],
+
+          coord[0] - HANDLE_LENGTH,
+          coord[1],
+          coord[0],
+          coord[1] + POINT_R
+        ]
+      }
+      if (el.classList.contains('out')) {
+        ghostPathDArguments.value = [
+          coord[0] + 2 * POINT_R,
+          coord[1] + POINT_R,
+          coord[0] + HANDLE_LENGTH,
+          coord[1],
+
+          coord[0] - HANDLE_LENGTH,
+          coord[1],
+          coord[0],
+          coord[1]
+        ]
+      }
     }
-    const handlePortMove = ({ ev, originEl, vm }: {ev:MouseEvent, originEl: HTMLElement, vm: any}) => {
-      console.log(ev.target, originEl, vm)
+    const handlePortMove = ({ ev, originEl, vm }: {ev:MouseEvent, originEl: SVGCircleElement, vm: any}) => {
       if (originEl.classList.contains('in')) {
         ghostPathDArguments.value = [
-          ev.pageX + POINT_R, ev.pageY, ev.pageX + HANDLE_LENGTH, ev.pageY,
+          ev.pageX, ev.pageY, ev.pageX + HANDLE_LENGTH, ev.pageY,
           ...ghostPathDArguments.value.slice(4)
         ]
       }
       if (originEl.classList.contains('out')) {
         ghostPathDArguments.value = [
           ...ghostPathDArguments.value.slice(0, 4),
-          ev.pageX - HANDLE_LENGTH, ev.pageY, ev.pageX - POINT_R, ev.pageY
+          ev.pageX - HANDLE_LENGTH, ev.pageY, ev.pageX, ev.pageY
         ]
       }
     }
-    const handlePortConnect = () => ({})
+    const handlePortConnect = ({ ev, originEl, vm }: {ev:MouseEvent, originEl: SVGCircleElement, vm: any}) => {
+      const el = ev.target as SVGGElement
+      const coord = [
+        el.getBoundingClientRect().x,
+        el.getBoundingClientRect().y
+      ]
+
+      if (originEl.classList.contains('in')) {
+        ghostPathDArguments.value = [
+          coord[0] + 2 * POINT_R,
+          coord[1] + POINT_R,
+          coord[0] + HANDLE_LENGTH,
+          coord[1],
+          ...ghostPathDArguments.value.slice(4)
+        ]
+      }
+      if (originEl.classList.contains('out')) {
+        ghostPathDArguments.value = [
+          ...ghostPathDArguments.value.slice(0, 4),
+          coord[0] - HANDLE_LENGTH,
+          coord[1],
+          coord[0],
+          coord[1] + POINT_R
+        ]
+      }
+    }
     const handlePortCancel = () => {
-      ghostPathDArguments.value = [0, 0, 0, 0, 0, 0, 0, 0]
+      ghostPathDArguments.value.fill(0)
       console.log('canceled')
     }
 
