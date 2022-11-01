9e8a2e46c75558deff270b052b25a2bf19245f73
gogoend <gogoend@qq.com>
2022-11-02 00:32:56 +0800

    refactor(canvas-staff): 将原nodes数组重构为对象，以id为key

--- src/store/canvasStuff.ts
+++ src/store/canvasStuff.ts
@@ -2,7 +2,7 @@ import { NodeInStore } from '@/schema/IoNode'
 import { Path } from '@/views/AppMain/components/SvgCanvas/type'
 import { Ref, ref, InjectionKey, provide } from 'vue'
 
-export const ALL_NODES_ON_CANVAS_SYMBOL: InjectionKey<Ref<NodeInStore[]>> = Symbol('Canvas上的所有节点')
+export const ALL_NODES_ON_CANVAS_SYMBOL: InjectionKey<Ref<Record<NodeInStore['id'], NodeInStore>>> = Symbol('Canvas上的所有节点')
 export const ADD_NODES_SYMBOL: InjectionKey<(node: NodeInStore) => void> = Symbol('添加节点函数')
 export const REMOVE_NODES_SYMBOL: InjectionKey<(nodeId: string) => void> = Symbol('移除节点函数')
 
@@ -18,34 +18,24 @@ export const ADD_RELATION_IN_MAP_INDEXED_BY_NODE_ID_SYMBOL: InjectionKey<(
 ) => void> = Symbol('向 节点id->出、入连线 的映射中添加连线关系')
 
 export default function canvasStuff() {
-  const nodes = ref<NodeInStore[]>([])
-  nodes.value = [
-    {
-      is: 'feTurbulence',
-      id: '91914e37-f574-52cd-9157-bdc30073796a',
-      position: [80, 30]
-    },
-    {
-      is: 'feDisplacementMap',
-      id: '45c3ab87-518c-5e30-89e6-910f9fa6f1dd',
-      position: [720, 640]
-    }
-  ]
+  const nodes = ref<Record<NodeInStore['id'], NodeInStore>>({})
+  nodes.value = {
+    '91914e37-f574-52cd-9157-bdc30073796a': { is: 'feTurbulence', id: '91914e37-f574-52cd-9157-bdc30073796a', position: [569, 462] },
+    '45c3ab87-518c-5e30-89e6-910f9fa6f1dd': { is: 'feDisplacementMap', id: '45c3ab87-518c-5e30-89e6-910f9fa6f1dd', position: [671, 111] },
+    'C63FCDE9-79FB-4243-90E1-D44258C8902D': { is: 'feOffset', id: 'C63FCDE9-79FB-4243-90E1-D44258C8902D', position: [1, 179] },
+    '4DEDA0C9-30A4-42FC-987F-A0F968C47631': { is: 'feTile', id: '4DEDA0C9-30A4-42FC-987F-A0F968C47631', position: [86, 587] },
+    '98538604-3FD4-4215-8ED9-A4B6882A9AE7': { is: 'feTurbulence', id: '98538604-3FD4-4215-8ED9-A4B6882A9AE7', position: [288, 264] },
+    '3566D34B-971F-4167-8DD4-BA6A4C4A302B': { is: 'feMerge', id: '3566D34B-971F-4167-8DD4-BA6A4C4A302B', position: [273, 32] }
+  }
   provide(ALL_NODES_ON_CANVAS_SYMBOL, nodes)
 
   const addNode = (node: NodeInStore) => {
-    nodes.value.push(node)
+    nodes.value[node.id] = node
   }
   provide(ADD_NODES_SYMBOL, addNode)
 
   const removeNode = (nodeId: string) => {
-    for (let i = 0; i < nodes.value.length;) {
-      if (nodeId === nodes.value[i].id) {
-        nodes.value.splice(i, 1)
-        return
-      }
-      i++
-    }
+    delete nodes.value[nodeId]
   }
   provide(REMOVE_NODES_SYMBOL, removeNode)
 
