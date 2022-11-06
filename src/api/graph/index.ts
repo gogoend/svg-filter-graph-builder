f856e27a6ec1038ea6ce66d387707e8f2d66d56d
gogoend <gogoend@qq.com>
2022-11-06 16:44:11 +0800

    refactor: 更新示例数据，以测试最近提交中加入的公共字符串、图像源节点

--- src/api/graph/index.ts
+++ src/api/graph/index.ts
@@ -4,14 +4,13 @@ export async function getNodes(): Promise<Record<NodeInStore['id'], NodeInStore>
   return new Promise(resolve => {
     setTimeout(
       () => {
-        resolve({
-          '91914e37-f574-52cd-9157-bdc30073796a': { is: 'feTurbulence', id: '91914e37-f574-52cd-9157-bdc30073796a', position: [569, 462] },
-          '45c3ab87-518c-5e30-89e6-910f9fa6f1dd': { is: 'feDisplacementMap', id: '45c3ab87-518c-5e30-89e6-910f9fa6f1dd', position: [671, 111] },
-          'C63FCDE9-79FB-4243-90E1-D44258C8902D': { is: 'feOffset', id: 'C63FCDE9-79FB-4243-90E1-D44258C8902D', position: [1, 179] },
-          '4DEDA0C9-30A4-42FC-987F-A0F968C47631': { is: 'feTile', id: '4DEDA0C9-30A4-42FC-987F-A0F968C47631', position: [86, 587] },
-          '98538604-3FD4-4215-8ED9-A4B6882A9AE7': { is: 'feTurbulence', id: '98538604-3FD4-4215-8ED9-A4B6882A9AE7', position: [288, 264] },
-          '3566D34B-971F-4167-8DD4-BA6A4C4A302B': { is: 'feMerge', id: '3566D34B-971F-4167-8DD4-BA6A4C4A302B', position: [273, 32] }
-        })
+        resolve(
+          {
+            'C63FCDE9-79FB-4243-90E1-D44258C8902D': { 'is': 'feOffset', 'id': 'C63FCDE9-79FB-4243-90E1-D44258C8902D', 'position': [484.00006103515625, 175.0000457763672] },
+            'CF377972-589A-40C1-ADD8-BF85A6B70D97': { 'is': 'SourceAlpha', 'id': 'CF377972-589A-40C1-ADD8-BF85A6B70D97', 'position': [238, 57] },
+            '5EC6CD3D-E50E-4BD9-B732-53B885A77FA3': { 'is': 'StringLiteral', 'id': '5EC6CD3D-E50E-4BD9-B732-53B885A77FA3', 'position': [231, 290] }
+          }
+        )
       },
       500
     )
@@ -24,39 +23,9 @@ export async function getLinks(): Promise<Record<LinkInStore['id'], LinkInStore>
       () => {
         resolve(
           {
-            '0': {
-              'id': '0',
-              'from': {
-                'vmId': 'C63FCDE9-79FB-4243-90E1-D44258C8902D',
-                'attr': 'result'
-              },
-              'to': {
-                'vmId': '4DEDA0C9-30A4-42FC-987F-A0F968C47631',
-                'attr': 'in'
-              }
-            },
-            '2': {
-              'id': '2',
-              'from': {
-                'vmId': '4DEDA0C9-30A4-42FC-987F-A0F968C47631',
-                'attr': 'result'
-              },
-              'to': {
-                'vmId': '45c3ab87-518c-5e30-89e6-910f9fa6f1dd',
-                'attr': 'in'
-              }
-            },
-            '3': {
-              'id': '3',
-              'from': {
-                'vmId': '98538604-3FD4-4215-8ED9-A4B6882A9AE7',
-                'attr': 'result'
-              },
-              'to': {
-                'vmId': '45c3ab87-518c-5e30-89e6-910f9fa6f1dd',
-                'attr': 'in2'
-              }
-            }
+            '81EF70DA-EAD8-400C-B28C-81AF36302FB8': { 'id': '81EF70DA-EAD8-400C-B28C-81AF36302FB8', 'from': { 'vmId': 'CF377972-589A-40C1-ADD8-BF85A6B70D97', 'attr': 'result' }, 'to': { 'vmId': 'C63FCDE9-79FB-4243-90E1-D44258C8902D', 'attr': 'in' }},
+            '24BB2B85-5C77-4493-93E1-8BFC569141C4': { 'id': '24BB2B85-5C77-4493-93E1-8BFC569141C4', 'from': { 'vmId': '5EC6CD3D-E50E-4BD9-B732-53B885A77FA3', 'attr': 'result' }, 'to': { 'vmId': 'C63FCDE9-79FB-4243-90E1-D44258C8902D', 'attr': 'dx' }},
+            'F7BD8DC4-4684-4023-BF6B-D47844B1F3F9': { 'id': 'F7BD8DC4-4684-4023-BF6B-D47844B1F3F9', 'from': { 'vmId': '5EC6CD3D-E50E-4BD9-B732-53B885A77FA3', 'attr': 'result' }, 'to': { 'vmId': 'C63FCDE9-79FB-4243-90E1-D44258C8902D', 'attr': 'dy' }}
           }
         )
       },
