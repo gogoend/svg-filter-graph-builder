708dfe28b9a0cb5eaf336e439dfbb676181e8ca3
gogoend <gogoend@qq.com>
2022-11-07 21:27:10 +0800

    feat(输出预览面板): 加入基本html结构

---
+++ src/views/AppMain/components/OutputPreviewPanel/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="output-preview-panel"
+  >
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, nextTick, inject, unref, computed } from 'vue'
+
+export default defineComponent({
+  name: 'OutputPreviewPanel',
+  components: { },
+  setup(props, { emit }) {
+    return {
+    }
+  }
+})
+</script>
+<style lang="scss" scoped>
+.output-preview-panel {
+  background-color: #fff;
+  box-shadow: 0 0 10px rgba(0,0,0,0.3);
+}
+</style>
