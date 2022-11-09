151ff5c7068e0cfbde6b9c69d81bfe25936b5321
gogoend <gogoend@qq.com>
2022-11-10 00:17:51 +0800

    fix(ci): 调整github pages ci脚本-构建前先重置工作区，并切换分支到`build/gh-page-src`

--- scripts/deploy-gh-pages.mjs
+++ scripts/deploy-gh-pages.mjs
@@ -2,6 +2,12 @@ import { execSync } from 'child_process'
 
 async function release() {
   execSync(
+    'git reset --hard'
+  )
+  execSync(
+    'git checkout build/gh-page-src'
+  )
+  execSync(
     'yarn version --patch --no-git-tag-version'
   )
   execSync(
