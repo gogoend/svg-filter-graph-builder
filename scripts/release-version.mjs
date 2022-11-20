0f1cc2684f75816131cd838ad453773e0ae87cd1
gogoend <gogoend@qq.com>
2022-11-21 01:49:47 +0800

    feat(ci): 加入release脚本

---
+++ scripts/release-version.mjs
@@ -0,0 +1,63 @@
+import { execSync } from 'child_process'
+import fs from 'fs'
+import path from 'path'
+
+const PATH_TO_PACKAGE_JSON = path.resolve('./package.json')
+const PATH_TO_RUNTIME_CONFIG_JSON = path.resolve('./public/runtime-config.json')
+
+const getPackageInfo = () => fs.readFileSync(
+  PATH_TO_PACKAGE_JSON
+)
+
+const getRuntimeConfig = () => fs.readFileSync(
+  PATH_TO_RUNTIME_CONFIG_JSON
+)
+
+async function release() {
+  execSync(
+    'yarn version --patch --no-git-tag-version'
+  )
+  const runtimeConfig = JSON.parse(getRuntimeConfig().toString('utf-8'))
+  const packageInfo = JSON.parse(getPackageInfo().toString('utf-8'))
+
+  runtimeConfig.name = packageInfo.name
+  runtimeConfig.version = packageInfo.version
+  runtimeConfig.buildVersion = typeof runtimeConfig.buildVersion === 'number' ? runtimeConfig.buildVersion + 1 : 1
+  runtimeConfig.buildTime = Number(new Date())
+  runtimeConfig.buildHash = execSync('git rev-parse HEAD').toString().trim()
+  fs.writeFileSync(
+    PATH_TO_RUNTIME_CONFIG_JSON,
+    JSON.stringify(
+      runtimeConfig,
+      null,
+      '  '
+    )
+  )
+  execSync(
+    'yarn'
+  )
+  execSync(
+    `git add -A`
+  )
+
+  const tagName = `v${packageInfo.version}`
+  execSync(
+    `git commit -m ${tagName}`
+  )
+  execSync(
+    `git tag ${tagName}`
+  )
+  execSync(
+    `git push origin ${tagName}`
+  )
+  try {
+    execSync(
+      'yarn build'
+    )
+  } catch (err) {
+    console.error(err)
+    throw err
+  }
+}
+
+release()
