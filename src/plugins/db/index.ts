9393ddcc08fc0699ea05abcf79628df1f7399ef3
gogoend <gogoend@qq.com>
2022-11-21 10:38:03 +0800

    refactor(配置迁移): 将原先从package.json引入的配置迁移到全局配置json文件中，以便打包时由打包程序更改此json；同时调整main.ts中各模块的引入顺序，初始化数据库、全局错误监听的代码用函数包裹，不再直接在模块根作用域下执行，直到全局配置json文件加载完后才执行；调整关于对话框文案-完善版本字段

--- src/plugins/db/index.ts
+++ src/plugins/db/index.ts
@@ -1,7 +1,13 @@
-import './init-gogoend-products-db'
+import initGogoendProductsDb from './init-gogoend-products-db'
+import initCurrentProductDb from './init-product-db'
 export {
   fileStorage,
   userProfileStorage,
   logsTable,
   appTable
 } from './init-product-db'
+
+export const initDb = async() => {
+  await initGogoendProductsDb()
+  await initCurrentProductDb()
+}
