d5ba88293255a66796cf5fd48794391fca4907a2
gogoend <gogoend@qq.com>
2022-11-21 12:03:08 +0800

    build(vue.config.js): 完善类型标柱；调整构建配置 生产包构建产物中不再包含sourceMap、console、debugger等相关内容

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
-// TODO: 如何区分 GitHub Actions 与 Vercel 打包环境？
-
+/**
+ * @type {import('@vue/cli-service').ProjectOptions}
+ */
 module.exports = {
-  publicPath: '/svg-filter-graph-builder/',
+  publicPath: '/svg-filter-graph-builder/', // TODO: 如何区分 GitHub Actions 与 Vercel 打包环境？
   css: {
     loaderOptions: {
       scss: {
@@ -10,11 +11,26 @@ module.exports = {
       }
     }
   },
+  productionSourceMap: false,
   chainWebpack(config) {
-    config.plugin('html').tap(args => {
-      args[0].title = 'SVG Filter Graph Builder (by @gogoend)'
-      return args
-    })
+    config.plugin('html')
+      .tap(args => {
+        args[0].title = 'SVG Filter Graph Builder (by @gogoend)'
+        return args
+      })
+      .end()
+      .optimization.minimizer('terser').tap((args) => {
+        Object.assign(
+          args[0].terserOptions.compress,
+          {
+            warnings: true,
+            drop_console: true,
+            drop_debugger: true
+          }
+        )
+        return args
+      })
+      .end()
   },
   pwa: {
     name: 'SVG Filter Graph Builder',
